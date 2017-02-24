102565bfd45aed2c57b3fe1971eb06e14a2d44dc
Dagony <dagony@gmail.com>
2017-02-24 14:38:13 +0100

    Displaying and creating posts with categories
    Added the categories that are generated from the categories table

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -21,7 +21,7 @@
 				<tr>
 					<td>{{ $post->id }}</td>
 					<td>{{ $post->user->name }}</td>
-					<td>{{ $post->category_id }}</td>
+					<td>{{ $post->category_id ? $post->category->name : 'Uncategorized' }}</td>
 					<td><img width="100" src="{{ $post->photo ? $post->photo->file : '/images/placeholder.png' }}" class="img-responsive img-rounded" /></td>
 					<td>{{ $post->title }}</td>
 					<td>{{ $post->body }}</td>
